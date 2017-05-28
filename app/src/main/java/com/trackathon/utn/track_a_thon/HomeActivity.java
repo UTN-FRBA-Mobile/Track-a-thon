e6508515340f6eb36574b21f94b53a603f5b2eb7
Federico Scarpa <fedescarpa@gmail.com>
2017-05-28 19:02:50 -0300

    added styles for navigation drawable

--- app/src/main/java/com/trackathon/utn/track_a_thon/HomeActivity.java
+++ app/src/main/java/com/trackathon/utn/track_a_thon/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.trackathon.utn.track_a_thon;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.trackathon.utn.track_a_thon.model.User;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,18 +20,10 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +33,12 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        User user = User.getCurrentUser();
+        View headerView = navigationView.getHeaderView(0);
+        ((TextView)headerView.findViewById(R.id.navigation_drawer_name)).setText(user.getName());
+        ((TextView)headerView.findViewById(R.id.navigation_drawer_email)).setText(user.getEmail());
+
     }
 
     @Override
