d78d4518938ebbf3795f04043ab99d04cf50cb7d
Ignacio Capuccio <ignacio.capuccio@gmail.com>
2017-06-04 21:09:51 -0300

    Add search feature in action bar

--- app/src/main/java/com/trackathon/utn/track_a_thon/RecycleViewRaceAdapter.java
+++ app/src/main/java/com/trackathon/utn/track_a_thon/RecycleViewRaceAdapter.java
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import com.trackathon.utn.track_a_thon.model.Race;
 import com.trackathon.utn.track_a_thon.model.RaceViewHolder;
@@ -13,15 +15,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.function.BiConsumer;
 
-class RecycleViewRaceAdapter extends RecyclerView.Adapter<RaceViewHolder> {
+class RecycleViewRaceAdapter extends RecyclerView.Adapter<RaceViewHolder> implements Filterable {
 
     private BiConsumer<String, Race> onClick;
     private HashMap<String, Race> races;
+    private HashMap<String, Race> filteredRaces;
     private List<String> keys;
+    private List<String> filteredKeys;
 
     RecycleViewRaceAdapter(HashMap<String, Race> races, BiConsumer<String, Race> onClick) {
         this.keys = new ArrayList<>(races.keySet());
+        this.filteredKeys = new ArrayList<>(races.keySet());
         this.races = races;
+        this.filteredRaces = races;
         this.onClick = onClick;
     }
 
@@ -33,8 +39,8 @@ class RecycleViewRaceAdapter extends RecyclerView.Adapter<RaceViewHolder> {
 
     @Override
     public void onBindViewHolder(RaceViewHolder raceViewHolder, int position) {
-        String raceId = this.keys.get(position);
-        Race race = this.races.get(raceId);
+        String raceId = this.filteredKeys.get(position);
+        Race race = this.filteredRaces.get(raceId);
 
         raceViewHolder.getRaceName().setText(race.getName());
         raceViewHolder.getRaceLocation().setText(race.getLocation());
@@ -54,7 +60,46 @@ class RecycleViewRaceAdapter extends RecyclerView.Adapter<RaceViewHolder> {
 
     @Override
     public int getItemCount() {
-        return races.size();
+        return filteredRaces.size();
     }
 
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+
+                String charString = charSequence.toString().toLowerCase();
+
+                if (charString.isEmpty()) {
+                    filteredKeys = keys;
+                    filteredRaces = races;
+                } else {
+                    List<String> auxKeys = new ArrayList<>();
+                    HashMap<String, Race> auxFilteredRaces = new HashMap<>();
+
+                    for (String key : keys) {
+                        Race race = races.get(key);
+                        if (race.getName().toLowerCase().contains(charString)) {
+                            auxKeys.add(key);
+                            auxFilteredRaces.put(key, race);
+                        }
+                    }
+                    filteredKeys = auxKeys;
+                    filteredRaces = auxFilteredRaces;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredRaces;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredRaces = (HashMap<String, Race>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
 }
