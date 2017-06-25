c96655dac6c17eee9240fe26d1fa1adf78f78970
Federico Scarpa <fedescarpa@gmail.com>
2017-06-25 17:31:45 -0300

    added RunnersTab

--- app/src/main/java/com/trackathon/utn/track_a_thon/RaceActivity.java
+++ app/src/main/java/com/trackathon/utn/track_a_thon/RaceActivity.java
@@ -1,5 +1,6 @@
 package com.trackathon.utn.track_a_thon;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
@@ -11,11 +12,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -33,6 +35,8 @@ import java.util.HashMap;
 
 public class RaceActivity extends AppCompatActivity {
 
+    private HashMap<String, Marker> runners = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +45,10 @@ public class RaceActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        Intent intent = getIntent();
+        String raceId = intent.getStringExtra(TrackatonConstant.RACE_ID);
+
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), raceId, runners);
 
         ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
@@ -52,28 +59,53 @@ public class RaceActivity extends AppCompatActivity {
     }
 
 
-    public static class PlaceholderFragment extends Fragment {
+    public static class RunnersFragment extends Fragment {
 
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        private String raceId;
+        private RecyclerView runnersRV;
+        private HashMap<String, Marker> runners;
 
-        public PlaceholderFragment() {
+        public RunnersFragment() {
         }
 
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static RunnersFragment newInstance(String raceId, HashMap<String, Marker> runners) {
+            RunnersFragment fragment = new RunnersFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putString(TrackatonConstant.RACE_ID, raceId);
+            args.putSerializable(TrackatonConstant.RUNNERS, runners);
             fragment.setArguments(args);
             return fragment;
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_race, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.fragment_race_runners, container, false);
+            raceId = getArguments().getString(TrackatonConstant.RACE_ID);
+            runners = (HashMap<String, Marker>) getArguments().getSerializable(TrackatonConstant.RUNNERS);
+            setRecyclerView(rootView);
             return rootView;
         }
+
+        public void setRecyclerView(View rootView) {
+            LinearLayoutManager racesLayout = new LinearLayoutManager(getContext());
+
+            runnersRV = (RecyclerView) rootView.findViewById(R.id.runners_rv);
+            runnersRV.setHasFixedSize(true);
+            runnersRV.setLayoutManager(racesLayout);
+
+            Firebase.allRunners(raceId, trackers -> {
+                RecycleViewRunnerAdapter adapter = new RecycleViewRunnerAdapter(trackers, (runnerId, runner) -> {
+                    Marker marker = runners.get(runner.getName());
+                    if (marker != null) {
+                        marker.showInfoWindow();
+                        // mMap.moveCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+                        // mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+                    }
+                });
+                runnersRV.setAdapter(adapter);
+            });
+        }
+
     }
 
     public static class MapViewFragment extends Fragment {
@@ -82,15 +114,16 @@ public class RaceActivity extends AppCompatActivity {
 
         private GoogleMap mMap;
         private String raceId;
-        private HashMap<String, Marker> runners = new HashMap<>();
+        private HashMap<String, Marker> runners;
 
         public MapViewFragment() {
         }
 
-        public static MapViewFragment newInstance(String raceId) {
+        public static MapViewFragment newInstance(String raceId, HashMap<String, Marker> runners) {
             MapViewFragment fragment = new MapViewFragment();
             Bundle args = new Bundle();
             args.putString(RACE_ID, raceId);
+            args.putSerializable(TrackatonConstant.RUNNERS, runners);
             fragment.setArguments(args);
             return fragment;
         }
@@ -98,7 +131,8 @@ public class RaceActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_race_map, container, false);
-            raceId = getArguments().getString(RACE_ID);
+            raceId = getArguments().getString(TrackatonConstant.RACE_ID);
+            runners = (HashMap<String, Marker>) getArguments().getSerializable(TrackatonConstant.RUNNERS);
             setMapView(rootView);
             return rootView;
         }
@@ -152,15 +186,20 @@ public class RaceActivity extends AppCompatActivity {
 
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        SectionsPagerAdapter(FragmentManager fm) {
+        private String raceId;
+        private HashMap<String, Marker> runners;
+
+        SectionsPagerAdapter(FragmentManager fm, String raceId, HashMap<String, Marker> runners) {
             super(fm);
+            this.raceId = raceId;
+            this.runners = runners;
         }
 
         @Override
         public Fragment getItem(int position) {
             return position == 0
-                    ? MapViewFragment.newInstance("Hello")
-                    : PlaceholderFragment.newInstance(position + 1);
+                    ? MapViewFragment.newInstance(raceId, runners)
+                    : RunnersFragment.newInstance(raceId, runners);
         }
 
         @Override
